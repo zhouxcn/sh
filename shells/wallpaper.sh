246019212489f38ef287cd33951c0f28321f5b91
zhouxc <zhouxc@v2uc.com>
2018-09-14 15:28:09 +0800

    modify

--- shells/wallpaper.sh
+++ shells/wallpaper.sh
@@ -28,7 +28,7 @@ downloadPath=/home/zhou/Pictures/${time}.jpg
 gsettings set org.gnome.desktop.background picture-uri "file:${downloadPath}"
 
 echo ""
-echo "有了美美的壁纸,接下来开始发呆吧..."
+echo "设置成功...开始发呆..."
 echo "呆"
 echo "呆"
 echo "呆"
